b4b711be84752cf533d3392ddebe319768f34217
Long-Soeng168 <generalpurpose167@gmail.com>
2025-02-26 19:58:41 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,12 @@ Route::post('products/{id}/update', [ProductController::class, 'update']);
 
 Route::apiResource('brands', controller: BrandController::class);
 Route::post('brands/{id}/update', [BrandController::class, 'update']);
+
+Route::post('/flmngr', function () {
+
+    \EdSDK\FlmngrServer\FlmngrServer::flmngrRequest(
+        array(
+            'dirFiles' => base_path() . '/public/files'
+        )
+    );
+});
